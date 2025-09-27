02afe673c133b6cc2b8aefefc5d5fd426ab755e3
Pierre Roland <127956402+pierreroland04@users.noreply.github.com>
2025-09-27 14:13:11 +0200

    feat: ajout de l'appel du backend depuis le front (en cours)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject  } from '@angular/core';
 import { MapComponent } from './component/map/map.component';
+import { HttpClient } from '@angular/common/http';
+
+interface Coordonne {
+  zoom: number;
+  center1: number;
+  center2: number;
+  country: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,7 +16,31 @@ import { MapComponent } from './component/map/map.component';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
+  value: String = "";
+
+  private http = inject(HttpClient);
+
   protected readonly title = signal('geo-find');
+
+  onInputChange(event: Event) {
+    this.value = (event.target as HTMLInputElement).value;
+    console.log(this.value);
+  }
+
+  onSubmit() {
+    console.log("entré");
+    const country = this.value; 
+
+    this.http.get<Coordonne>(`/maps/${country}`).subscribe({
+      next: (config) => {
+        console.log('Réponse backend:', config);
+      },
+      error: (err) => {
+        console.error('Erreur API:', err);
+      }
+    });
+  }
 }
 
